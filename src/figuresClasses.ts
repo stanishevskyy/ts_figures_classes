fecf490abda6b8bfd4c9b92b148907544a27db6e
Your_Name Your_Surname <your@email>
2024-11-28 13:46:44 +0200

    add structure triangle

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,41 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: 'triangle' = 'triangle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error(`These sides can not be a less than zero`);
+    }
+
+    if (this.a + this.b <= this.c) {
+      throw new Error(
+        `The sides ${this.a} and ${this.b} can not be less ${this.c}`,
+      );
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const result = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return result;
+  }
+}
 
 export class Circle implements Figure {}
 
 export class Rectangle implements Figure {}
 
-export function getInfo(figure): string {
-  return typeof figure;
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
